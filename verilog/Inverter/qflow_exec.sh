4705fa022f2ff057c5be52c0b890badcdf0761bd
s-aarif <802aarif@gmail.com>
2025-11-03 16:23:35 +0530

    Remove verilog folder from repository

--- verilog/Inverter/qflow_exec.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/tcsh -f
-#-------------------------------------------
-# qflow exec script for project ~/Documents/studies/Program/verilog/Inverter
-#-------------------------------------------
-
-# /usr/local/share/qflow/scripts/yosys.sh ~/Documents/studies/Program/verilog/Inverter inverter ~/Documents/studies/Program/verilog/Inverter/source/inverter_tb.v || exit 1
-# 
